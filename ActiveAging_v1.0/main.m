c92e910ce50327d0430d38444f0379b1b7826f27
kwokpingl <kwokpinglau.119@gmail.com>
2017-01-09 21:48:49 +0800

    This is the very first version of our APP Active Aging

---
+++ ActiveAging_v1.0/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ActiveAging_v1.0
+//
+//  Created by Kwok Ping Lau on 12/26/16.
+//  Copyright © 2016 PING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
